d3cfb4d0f7b0a7f89c70bdb947ee330626570452
Thoriq Fatihassalam <129358222+ThoriqFatih@users.noreply.github.com>
2023-11-06 09:52:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-   
     const pokemonList = data.results;
     const pokemonContainer = document.getElementById('pokemonContainer');
+
     pokemonList.forEach(pokemon => {
 
       const cardDiv = document.createElement('div');
       cardDiv.classList.add('col-xl-3', 'col-md-6', 'mb-4');
+
       const cardContent = document.createElement('div');
       cardContent.classList.add('card', 'border-primary', 'shadow', 'h-100', 'py-2');
+
       const cardBody = document.createElement('div');
-      cardBody.classList.add('card-body');
-      const row = document.createElement('div');
-      row.classList.add('row', 'no-gutters', 'align-items-center');
-      const col = document.createElement('div');
-      col.classList.add('col', 'mr-2');
-      col.textContent = pokemon.name;
+      cardBody.classList.add('card-body', 'd-flex', 'flex-column', 'align-items-center');
+
+      const pokemonName = document.createElement('p');
+      pokemonName.classList.add('mb-2');
+      pokemonName.textContent = pokemon.name;
 
       const detailButton = document.createElement('a');
       detailButton.href = '#';
       detailButton.classList.add('btn', 'btn-primary');
       detailButton.textContent = 'Detail';
 
-      row.appendChild(col);
-      row.appendChild(detailButton);
-      cardBody.appendChild(row);
+      cardBody.appendChild(pokemonName);
+      cardBody.appendChild(detailButton);
       cardContent.appendChild(cardBody);
       cardDiv.appendChild(cardContent);
 
@@ -37,5 +37,3 @@ fetch(apiUrl)
   .catch(error => {
     console.error('Error fetching data:', error);
   });
-    
-  
